b47fa7cc10f97b9aa27e774bce23c7a3fb7e9894
Pyry Ahlfors <pyry.ahlfors@sc5.io>
2018-10-31 08:07:21 +0200

    updated touch event

--- js/main.js
+++ js/main.js
@@ -138,7 +138,15 @@
 		elem.top = elem.elemBounding.top;
 		elem.left = elem.elemBounding.left;
 
-		params.container.addEventListener('mousemove', function(e){
+		this.touchEvent = (function(){
+			var testTouch = document.createElement("DIV");
+			testTouch.setAttribute('ontouchstart', 'return;');
+			var isTouchDevice = (typeof testTouch.ontouchstart == 'function' && window.screenX === 0) ? true : false;
+			return (isTouchDevice) ? 'touchstart' : 'mousemove';
+			})();
+
+
+		params.container.addEventListener(this.touchEvent, function(e){
 			e.preventDefault();
 			let rotaX = (20/elem.width) * (elem.width/2 -(e.pageX -elem.left));
 			let rotaY = (20/elem.height) * (elem.height/2 -(e.pageY -elem.top));
