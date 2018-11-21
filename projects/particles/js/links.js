5177e7330223a5edfd422fcb91bcd9c3288c2bc4
Pyry Ahlfors <pyry.ahlfors@sc5.io>
2018-11-21 09:35:54 +0200

    ...

--- projects/particles/js/links.js
+++ projects/particles/js/links.js
@@ -2,19 +2,19 @@ let linksContainer = document.createElement("DIV");
 linksContainer.className = 'links-holder';
 
 const linkArray = {
-  Starfield : '/particles/index.html',
-  Sand : '/particles/sand.html',
-  Sperm : '/particles/sperm.html',
-  Electricity : '/particles/electricity.html',
-  Plasma : '/particles/plasma.html',
-  Medusa : '/particles/medusa.html',
-  Wormhole : '/particles/wormhole.html',
-  Walker : '/particles/walker.html'
+  Starfield : 'particles/index.html',
+  Sand : 'particles/sand.html',
+  Sperm : 'particles/sperm.html',
+  Electricity : 'particles/electricity.html',
+  Plasma : 'particles/plasma.html',
+  Medusa : 'particles/medusa.html',
+  Wormhole : 'particles/wormhole.html',
+  Walker : 'particles/walker.html'
 };
 
 Object.keys(linkArray).forEach((a) => {
 	const experimentLink = document.createElement("A");
-	experimentLink.setAttribute('href', `${linkArray[a]}`);
+	experimentLink.setAttribute('href', `/projects/${linkArray[a]}`);
 	experimentLink.appendChild(document.createTextNode(a));
 	linksContainer.appendChild(experimentLink);
 	linksContainer.appendChild(document.createElement('BR'));
