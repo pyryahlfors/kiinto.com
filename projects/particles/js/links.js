a7a62d13a474ab35dc0d3b78b5f1f19e439ea49b
Pyry Ahlfors <pyry.ahlfors@sc5.io>
2018-11-21 09:34:13 +0200

    sdgs

--- projects/particles/js/links.js
+++ projects/particles/js/links.js
@@ -2,14 +2,14 @@ let linksContainer = document.createElement("DIV");
 linksContainer.className = 'links-holder';
 
 const linkArray = {
-  Starfield : 'index.html',
-  Sand : 'sand.html',
-  Sperm : 'sperm.html',
-  Electricity : 'electricity.html',
-  Plasma : 'plasma.html',
-  Medusa : 'medusa.html',
-  Wormhole : 'wormhole.html',
-  Walker : 'walker.html'
+  Starfield : '/projects/particles/index.html',
+  Sand : '/projects/particles/sand.html',
+  Sperm : '/projects/particles/sperm.html',
+  Electricity : '/projects/particles/electricity.html',
+  Plasma : '/projects/particles/plasma.html',
+  Medusa : '/projects/particles/medusa.html',
+  Wormhole : '/projects/particles/wormhole.html',
+  Walker : '/projects/particles/walker.html'
 };
 
 Object.keys(linkArray).forEach((a) => {
