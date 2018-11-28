83c001271cdd8208cfaaf03ffd64efe8bd3a2aed
Pyry Ahlfors <pyry.ahlfors@sc5.io>
2018-11-28 09:29:56 +0200

    analytics

--- projects/particles/js/links.js
+++ projects/particles/js/links.js
@@ -13,6 +13,26 @@ const linkArray = {
   Walker : 'particles/walker.html'
 };
 
+let navigationToggle = document.createElement('span');
+navigationToggle.className = 'menu-trigger';
+navigationToggle.addEventListener('click', () => {document.body.classList.toggle('nav-open')}, false);
+
+let navIconContainer = document.createElement('span');
+let barTop = document.createElement('i');
+barTop.className = 'menu-trigger-bar top';
+let barMiddle = document.createElement('i');
+barMiddle.className = 'menu-trigger-bar middle';
+let barBottom = document.createElement('i');
+barBottom.className = 'menu-trigger-bar bottom';
+
+navIconContainer.appendChild(barTop);
+navIconContainer.appendChild(barMiddle);
+navIconContainer.appendChild(barBottom);
+
+navigationToggle.appendChild(navIconContainer);
+
+document.body.appendChild(navigationToggle);
+
 Object.keys(linkArray).forEach((a) => {
 	const experimentLink = document.createElement("A");
 	experimentLink.setAttribute('href', `/projects/${linkArray[a]}`);
@@ -21,4 +41,4 @@ Object.keys(linkArray).forEach((a) => {
 	linksContainer.appendChild(document.createElement('BR'));
 	});
 
-document.body.appendChild(linksContainer)
+document.body.appendChild(linksContainer);
